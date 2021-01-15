b6401d221593727a410f9aad837686b95d334f37
Norbert Songin <norbertsongin@users.noreply.github.com>
2021-01-15 17:09:59 +0100

    Increase number of words in Content

--- src/components/Content.tsx
+++ src/components/Content.tsx
@@ -1,23 +1,23 @@
 import React from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 import Chance from 'chance';
+import {ScrollView} from 'react-native-gesture-handler';
 
 const chance = new Chance();
 
 export const Content = () => {
-  const text = chance.sentence({words: 100});
+  const text = chance.sentence({words: 1000});
 
   return (
-    <View style={styles.container}>
+    <ScrollView contentContainerStyle={styles.container}>
       <Text style={styles.contentText}>{text}</Text>
-    </View>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'flex-end',
+    padding: 20,
   },
   contentText: {
     color: 'white',
